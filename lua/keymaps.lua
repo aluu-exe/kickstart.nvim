8af721760712c9336502ac66d508a6c37dbebb2f
aluu-exe <78183008+aluu-exe@users.noreply.github.com>
2024-05-26 18:15:56 +1000

    moves vim keymaps and options out of the init.lua file

---
+++ lua/keymaps.lua
@@ -0,0 +1,49 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Modify arrow keys to suit split keebs
+vim.cmd [[
+    nnoremap h ;
+
+    nnoremap j h
+    nnoremap k j
+    nnoremap l k
+    nnoremap ; l
+
+    vnoremap j h
+    vnoremap k j
+    vnoremap l k
+    vnoremap ; l
+]]
+
+-- primeagen remaps
+-- sets it to return to netrw (project view) view
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+-- moves visual lines with capital K and L
+vim.keymap.set('v', 'K', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'L', ":m '<-2<CR>gv=gv")
+
+-- keeps your cursor at the same place after appending line below
+vim.keymap.set('n', 'J', 'mzJ`z')
+-- keeps c-d and c-u centered
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- allows search terms to say centered
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- greatest remap ever
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+-- these do things around system clipboard
+vim.keymap.set('n', '<leader>y', [["+y]])
+vim.keymap.set('v', '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+-- vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- llows you to replace whatever word you're already on
+vim.keymap.set('n', '<leader>l', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Rep[L]aces the word currently on' })
+
+-- turns current file to an executable
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
