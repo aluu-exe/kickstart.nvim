e37cf61b9b8eaf27a42c548e23837217b1a98bcc
aluu-exe <78183008+aluu-exe@users.noreply.github.com>
2024-05-27 17:02:14 +1000

    changes theme to storm from night

--- init.lua
+++ init.lua
@@ -730,8 +730,8 @@ require('lazy').setup({
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- any other, such as 'tokyonight-night', 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
