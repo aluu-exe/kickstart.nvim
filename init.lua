35b1f16000d4154af71129f13dc44aa165888779
aluu-exe <78183008+aluu-exe@users.noreply.github.com>
2024-05-27 12:16:40 +1000

    enables nerdfont

--- init.lua
+++ init.lua
@@ -98,7 +98,7 @@ require 'options'
 require 'keymaps'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
